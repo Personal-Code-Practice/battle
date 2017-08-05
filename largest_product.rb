b153a6b68f7e1d97da70a150fdbefc3f5f657c1a
jasmine <jasliu2012@gmail.com>
2017-08-05 14:20:02 -0500

    Solved how to output maximum product of adjacent elements of an array with each_cons and .max

---
+++ largest_product.rb
@@ -0,0 +1,27 @@
+input_array = [3, 6, -2, -5, 7, 3]
+
+def adjacent_elements_product(input_array)
+  product_array = []
+  input_array.each_cons(2).map do |a, b|
+    product_array << b * a
+  end
+  p product_array.max
+end
+
+adjacentElementsProduct(inputArray)
+
+
+#RUBY DOC REFERENCE:
+
+(1..10).each_cons(3) {|a| p a}
+#=> 
+[1, 2, 3]
+[2, 3, 4]
+[3, 4, 5]
+[4, 5, 6]
+[5, 6, 7]
+[6, 7, 8]
+[7, 8, 9]
+[8, 9, 10]
+
+
