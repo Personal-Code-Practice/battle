eb51f60e4cfa191db295eea4ffeea0367bf2dbf1
jasmine <jasliu2012@gmail.com>
2017-08-05 18:08:04 -0500

    Testing sequence [1, 3, 2, 1]. Should return false, but returns true. Likely because loop doesn't take into consideration last element.

--- almost_increasing_sequence.rb
+++ almost_increasing_sequence.rb
@@ -3,20 +3,18 @@
 
 #if the previous number is not less than the next number, then return false. 
 
-sequence = [1, 3, 2]
+sequence = [1, 3, 2, 1]
 
 def almost_increasing_sequence(sequence)
   count = 0
   sequence.each_cons(2).map do |a, b|
-    until sequence[sequence.count-1]
-      if a > b
-        sequence.delete_if { |a| a > b }
-        p "deleted"
-        count += 1
-        p count
-      else 
-        true
-      end
+    if a > b
+      sequence.delete_if { |a| a > b }
+      # p "deleted"
+      count += 1
+      # p count
+    else 
+      true
     end
   end
   if count > 1
