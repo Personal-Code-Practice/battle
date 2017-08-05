3ee64e9657f0f42401101d05f4725e2353ca4335
jasmine <jasliu2012@gmail.com>
2017-08-05 17:38:38 -0500

    partially solved the increasing sequence puzzle. It appears the code somehow rules out the last number of the sequence, which is a huge determinant in whether it is possible to remove only one element of the array to obtain a strictly increasing sequence.

---
+++ almost_increasing_sequence.rb
@@ -0,0 +1,29 @@
+#input: array.integer sequence
+#output: boolean (true/false). Return true if it is possible to remove one element from the array in order to get a strictly increasing sequence, otherwise return false.
+
+#if the previous number is not less than the next number, then return false. 
+
+sequence = [1, 3, 2]
+
+def almost_increasing_sequence(sequence)
+  count = 0
+  sequence.each_cons(2).map do |a, b|
+    until sequence[sequence.count-1]
+      if a > b
+        sequence.delete_if { |a| a > b }
+        p "deleted"
+        count += 1
+        p count
+      else 
+        true
+      end
+    end
+  end
+  if count > 1
+    p false
+  else
+    p true
+  end
+end
+
+almost_increasing_sequence(sequence)
